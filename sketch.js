5074cfee46de2285f8a29df2feba745ca57292de
dhivyaankumaran <85639664+dhivyaankumaran@users.noreply.github.com>
2021-06-23 22:06:07 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var path, boy, leftBoundary, rightBoundary;
+var pathImage, boyImage;
+
+function preload(){
+  //pre-load images
+  pathImage=loadImage("path.png");
+  boyImage=loadAnimation("Runner-1.png", "Runner-2.png");
+ 
+}
+
+function setup(){
+  createCanvas(400,400);
+  path=createSprite(200,200);
+  path.addImage(pathImage);
+  path.velocityY = 4;
+  path.scale=1.2;
+  
+  boy=createSprite(180,340, 30, 30);
+  boy.addAnimation("running", boyImage); 
+  boy.scale=0.08;
+
+  leftBoundary=createSprite(0,0,100,800);
+  leftBoundary.visible = false;
+
+  rightBoundary=createSprite(410,0,100,800);
+  rightBoundary.visible = false;
+}
+
+function draw() {
+  background(0);
+  path.velocityY=4;
+  boy.x=mouseX;
+
+  edges= createEdgeSprites(); 
+  boy.collide(edges[3]); 
+  boy.collide(leftBoundary); 
+  boy.collide(rightBoundary); 
+
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+  drawSprites();
+}
